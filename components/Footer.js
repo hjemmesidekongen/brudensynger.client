13d3ae3d42628da87f04533290469979806aa674
Morten Nissen <mail@hjemmesidekongen.dk>
2019-10-24 21:45:53 +0200

    Add footer navigation

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import FooterNavigation from './FooterNavigation';
+import { secondaryLinks } from '../globals/navigation';
+
 const Footer = () => {
   return (
     <footer>
       <div className="container">
         2008-{new Date().getFullYear()} © brudensynger.dk |{' '}
         <a href="mailto:support@brudensynger.dk">support@brudensynger.dk</a>
+        <FooterNavigation links={secondaryLinks} />
       </div>
     </footer>
   );
