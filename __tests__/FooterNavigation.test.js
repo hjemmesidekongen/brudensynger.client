5c043eef6f20483df8eac4178ce63a7a5926af98
Morten Nissen <mail@hjemmesidekongen.dk>
2019-10-25 08:03:18 +0200

    Move tests to seperate folder __tests__

--- __tests__/FooterNavigation.test.js
+++ __tests__/FooterNavigation.test.js
@@ -1,29 +1,28 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../utils/TestUtils';
-import HeaderNavigation from './HeaderNavigation';
+import { findByTestAttr } from '../utils/testUtils';
+import FooterNavigation from './FooterNavigation';
 
 it('renders without error', () => {
-  const component = shallow(<HeaderNavigation links={[]} />);
+  const component = shallow(<FooterNavigation />);
 
   expect(component.length).toBe(1);
 });
 
-it('doesnt render navigation if no links are provided', () => {
-  const component = shallow(<HeaderNavigation links={[]} />);
-  const navigation = findByTestAttr(component, 'navigation');
+it('doesnt render navigation if no links are passed in', () => {
+  const component = shallow(<FooterNavigation links={[]} />);
+  const navigation = findByTestAttr(component, 'test');
 
   expect(navigation.length).toBe(0);
 });
-
-it('renders all links that are passed via props', () => {
+it('renders all links passed in via props', () => {
   const linkProps = [
     { title: 'Link 1', path: '/1' },
     { title: 'Link 2', path: '/2' },
     { title: 'Link 3', path: '/3' },
   ];
-  const component = shallow(<HeaderNavigation links={linkProps} />);
+  const component = shallow(<FooterNavigation links={linkProps} />);
   const linkElements = findByTestAttr(component, 'link');
 
   expect(linkElements.length).toBe(linkProps.length);
@@ -31,7 +30,7 @@ it('renders all links that are passed via props', () => {
 
 it('throws an error if passed props have the wrong shape', () => {
   const linkProps = [{ wrongKey: 'me' }];
-  const component = () => shallow(<HeaderNavigation links={linkProps} />);
+  const component = () => shallow(<FooterNavigation links={linkProps} />);
 
   expect(component).toThrowError();
 });
--- __tests__/HeaderNavigation.test.js
+++ __tests__/HeaderNavigation.test.js
@@ -1,28 +1,29 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../utils/TestUtils';
-import FooterNavigation from './FooterNavigation';
+import { findByTestAttr } from '../utils/testUtils';
+import HeaderNavigation from './HeaderNavigation';
 
 it('renders without error', () => {
-  const component = shallow(<FooterNavigation />);
+  const component = shallow(<HeaderNavigation links={[]} />);
 
   expect(component.length).toBe(1);
 });
 
-it('doesnt render navigation if no links are passed in', () => {
-  const component = shallow(<FooterNavigation links={[]} />);
-  const navigation = findByTestAttr(component, 'test');
+it('doesnt render navigation if no links are provided', () => {
+  const component = shallow(<HeaderNavigation links={[]} />);
+  const navigation = findByTestAttr(component, 'navigation');
 
   expect(navigation.length).toBe(0);
 });
-it('renders all links passed in via props', () => {
+
+it('renders all links that are passed via props', () => {
   const linkProps = [
     { title: 'Link 1', path: '/1' },
     { title: 'Link 2', path: '/2' },
     { title: 'Link 3', path: '/3' },
   ];
-  const component = shallow(<FooterNavigation links={linkProps} />);
+  const component = shallow(<HeaderNavigation links={linkProps} />);
   const linkElements = findByTestAttr(component, 'link');
 
   expect(linkElements.length).toBe(linkProps.length);
@@ -30,7 +31,7 @@ it('renders all links passed in via props', () => {
 
 it('throws an error if passed props have the wrong shape', () => {
   const linkProps = [{ wrongKey: 'me' }];
-  const component = () => shallow(<FooterNavigation links={linkProps} />);
+  const component = () => shallow(<HeaderNavigation links={linkProps} />);
 
   expect(component).toThrowError();
 });
